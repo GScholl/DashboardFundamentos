276b7330d23611f589872db8555c99dfeff2f817
Guilherme Scholl <108899269+GScholl@users.noreply.github.com>
2023-10-13 14:15:13 -0300

    Alterações nos graficos

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sites Populares </title>
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         @keyframes chartjs-render-animation {
@@ -62,94 +63,125 @@
     ?>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-6">
                 <div class="card">
                     <div class="card-header border-0">
                         <div class="d-flex justify-content-between">
-                            <h3 class="card-title">Média Salárial X Custo do plano pago</h3>
-                            
+                            <h3 class="card-title">Média Salárial </h3>
+
                         </div>
                     </div>
                     <div class="card-body">
                         <div class="d-flex">
                             <p class="d-flex flex-column">
-                               
+
                                 <span></span>
                             </p>
                             <p class="ml-auto d-flex flex-column text-right">
                                 <span class="text-success">
-                                    <i class="fas fa-arrow-up"></i> 33.1%
+                                    <i class="fas fa-arrow-up"></i>
                                 </span>
-                                <span class="text-muted">Since last month</span>
+                                <span class="text-muted"></span>
                             </p>
                         </div>
 
                         <div class="position-relative mb-4">
-                            <canvas id="sales-chart" height="200"></canvas>
+                            <canvas id="chart-salario" style="height:60vh;"></canvas>
                         </div>
                         <div class="d-flex flex-row justify-content-end">
-                            
+
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-header border-0">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="card-title">Custo do plano pago</h3>
 
-                <div class="col-12">
-                    <table class="table table-striped table-bordered table-responsive">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nome Do site</th>
-                                <th scope="col">Url</th>
-                                <th scope="col">Custo</th>
-                                <th scope="col">Requisitos</th>
-                                <th scope="col">Salário</th>
-                                <th scope="col">Tipo do trabalho</th>
-                                <th scope="col">Caracteristicas</th>
-                                <th scope="col">Vantagens</th>
-                                <th scope="col">Desvantagens</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <p class="d-flex flex-column">
 
-                            <?php
+                                <span></span>
+                            </p>
+                            <p class="ml-auto d-flex flex-column text-right">
+                                <span class="text-success">
+                                    <i class="fas fa-arrow-up"></i>
+                                </span>
+                                <span class="text-muted"></span>
+                            </p>
+                        </div>
 
-                            $qtd = $res->num_rows;
-                            if ($qtd > 0) {
+                        <div class="position-relative mb-4">
+                            <canvas id="chart-custo" style="height:60vh;"></canvas>
+                        </div>
+                        <div class="d-flex flex-row justify-content-end">
 
-
-
-                                while ($row = $res->fetch_object()) { ?>
-                                    <tr>
-                                        <th scope="row"><?= $row->SiteID ?> </th>
-                                        <td colspan="1"><?= $row->NomeSite ?> </td>
-                                        <td><a href="<?= $row->URL ?>"><?= $row->URL ?></a></td>
-                                        <td colspan="1">R$ <?= number_format($row->CustoUtilizacao, 2, ',', '.') ?> </td>
-                                        <td><?= $row->Requisitos ?></td>
-                                        <td colspan="1"> <?= $row->Salario ?> </td>
-                                        <td><?= $row->TipoTrabalho ?></td>
-                                        <td colspan="1"><?= $row->Caracteristicas ?> </td>
-                                        <td><?= $row->Vantagens ?></td>
-                                        <td colspan="1"><?= $row->Desvantagens ?> </td>
-
-                                    </tr>
-
-                                <?php
-                                }
-                            } else { ?>
-
-                                <td colspan="11">Não possui nenhum registro de site</td>
-
-                            <?php } ?>
-
-                        </tbody>
-                    </table>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-12">
+                <table class="table table-striped table-bordered table-responsive">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome Do site</th>
+                            <th scope="col">Url</th>
+                            <th scope="col">Custo</th>
+                            <th scope="col">Requisitos</th>
+                            <th scope="col">Salário</th>
+                            <th scope="col">Tipo do trabalho</th>
+                            <th scope="col">Caracteristicas</th>
+                            <th scope="col">Vantagens</th>
+                            <th scope="col">Desvantagens</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php
+
+                        $qtd = $res->num_rows;
+                        if ($qtd > 0) {
+
+
+
+                            while ($row = $res->fetch_object()) { ?>
+                                <tr>
+                                    <th scope="row"><?= $row->SiteID ?> </th>
+                                    <td colspan="1"><?= $row->NomeSite ?> </td>
+                                    <td><a href="<?= $row->URL ?>"><?= $row->URL ?></a></td>
+                                    <td colspan="1">R$ <?= number_format($row->CustoUtilizacao, 2, ',', '.') ?> </td>
+                                    <td><?= $row->Requisitos ?></td>
+                                    <td colspan="1"> <?= $row->Salario ?> </td>
+                                    <td><?= $row->TipoTrabalho ?></td>
+                                    <td colspan="1"><?= $row->Caracteristicas ?> </td>
+                                    <td><?= $row->Vantagens ?></td>
+                                    <td colspan="1"><?= $row->Desvantagens ?> </td>
+
+                                </tr>
+
+                            <?php
+                            }
+                        } else { ?>
+
+                            <td colspan="11">Não possui nenhum registro de site</td>
+
+                        <?php } ?>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script src="js/chart.min.js"></script>
-        <script src="js/script.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="js/chart.min.js"></script>
+    <script src="js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 
--- js/script.js
+++ js/script.js
@@ -1,89 +1,182 @@
-let Sites = [];
-
 $.ajax({
   url: `dados.php`,
   method: "GET",
   success: function (data) {
-    Sites = JSON.parse(data);
-    console.log(data);
+    let Sites = JSON.parse(data);
+    console.log(Sites);
+    geradorDeChart(Sites);
   },
   error: function (data) {},
 });
-
-$(function () {
-  "use strict";
-  var ticksStyle = {
-    fontColor: "#495057",
-    fontStyle: "bold",
-  };
-  var mode = "index";
-  var intersect = true;
-  var $salesChart = $("#sales-chart");
-  var salesChart = new Chart($salesChart, {
-    type: "bar",
-    data: {
-      labels: [Sites[0]['NomeSite'], Sites[1]['NomeSite'], Sites[2]['NomeSite'], Sites[3]['NomeSite'],Sites[4]['NomeSite']],
-      datasets: [
-        {
-          backgroundColor: "#007bff",
-          borderColor: "#007bff",
-          data: [1000, 2000, 3000, 2500, 2700, 2500, 3000],
-        },
-        {
-          backgroundColor: "#ced4da",
-          borderColor: "#ced4da",
-          data: [700, 1700, 2700, 2000, 1800, 1500, 2000],
-        },
-      ],
-    },
-    options: {
-      maintainAspectRatio: false,
-      tooltips: {
-        mode: mode,
-        intersect: intersect,
-      },
-      hover: {
-        mode: mode,
-        intersect: intersect,
-      },
-      legend: {
-        display: false,
-      },
-      scales: {
-        yAxes: [
+const geradorDeChart = (Sites) => {
+  $(function () {
+    "use strict";
+    var ticksStyle = {
+      fontColor: "#495057",
+      fontStyle: "bold",
+    };
+    var mode = "index";
+    var intersect = true;
+    var $salesChart = $("#chart-custo");
+    var salesChart = new Chart($salesChart, {
+      type: "bar",
+      data: {
+        labels: [
+          Sites[0].NomeSite,
+          Sites[1].NomeSite,
+          Sites[2].NomeSite,
+          Sites[3].NomeSite,
+          Sites[4].NomeSite,
+        ],
+        datasets: [
+      
           {
-            gridLines: {
-              display: true,
-              lineWidth: "4px",
-              color: "rgba(0, 0, 0, .2)",
-              zeroLineColor: "transparent",
-            },
-            ticks: $.extend(
-              {
-                beginAtZero: true,
-                callback: function (value) {
-                  if (value >= 1000) {
-                    value /= 1000;
-                    value += "k";
-                  }
-                  return "$" + value;
-                },
+            backgroundColor: "#007bff",
+            borderColor: "#007bff",
+            data: [
+              Sites[0].CustoUtilizacao,
+              Sites[1].CustoUtilizacao,
+              Sites[2].CustoUtilizacao,
+              Sites[3].CustoUtilizacao,
+              Sites[4].CustoUtilizacao,
+            ],
+          },
+          
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        tooltips: {
+          mode: mode,
+          intersect: intersect,
+        },
+        hover: {
+          mode: mode,
+          intersect: intersect,
+        },
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                lineWidth: "4px",
+                color: "rgba(0, 0, 0, .2)",
+                zeroLineColor: "transparent",
               },
-              ticksStyle
-            ),
-          },
-        ],
-        xAxes: [
-          {
-            display: true,
-            gridLines: {
-              display: false,
+              ticks: $.extend(
+                {
+                  beginAtZero: true,
+                  callback: function (value) {
+                    if (value >= 1000) {
+                      value /= 1000;
+                      value += " Mil";
+                    }
+                    return "$" + value;
+                  },
+                },
+                ticksStyle
+              ),
             },
-            ticks: ticksStyle,
+          ],
+          xAxes: [
+            {
+              display: true,
+              gridLines: {
+                display: false,
+              },
+              ticks: ticksStyle,
+            },
+          ],
+        },
+      },
+    });
+  });
+  $(function () {
+    "use strict";
+    var ticksStyle = {
+      fontColor: "#495057",
+      fontStyle: "bold",
+    };
+    var mode = "index";
+    var intersect = true;
+    var $chartSalario = $("#chart-salario");
+    var chartSalario = new Chart($chartSalario, {
+      type: "bar",
+      data: {
+        labels: [
+          Sites[0].NomeSite,
+          Sites[1].NomeSite,
+          Sites[2].NomeSite,
+          Sites[3].NomeSite,
+          Sites[4].NomeSite,
+        ],
+        datasets: [
+          {
+            backgroundColor: "#ced4da",
+            borderColor: "#ced4da",
+            data: [
+              Sites[0].Salario,
+              Sites[1].Salario,
+              Sites[2].Salario,
+              Sites[3].Salario,
+              Sites[4].Salario,
+            ],
           },
+         
+          
         ],
       },
-    },
+      options: {
+        maintainAspectRatio: false,
+        tooltips: {
+          mode: mode,
+          intersect: intersect,
+        },
+        hover: {
+          mode: mode,
+          intersect: intersect,
+        },
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                lineWidth: "4px",
+                color: "rgba(0, 0, 0, .2)",
+                zeroLineColor: "transparent",
+              },
+              ticks: $.extend(
+                {
+                  beginAtZero: true,
+                  callback: function (value) {
+                    if (value >= 1000) {
+                      value /= 1000;
+                      value += " Mil";
+                    }
+                    return "$" + value;
+                  },
+                },
+                ticksStyle
+              ),
+            },
+          ],
+          xAxes: [
+            {
+              display: true,
+              gridLines: {
+                display: false,
+              },
+              ticks: ticksStyle,
+            },
+          ],
+        },
+      },
+    });
   });
- 
-});
+};
